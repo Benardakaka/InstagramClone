1402086feccac150081929e76c9aaeda5833f87c
Benardakaka <benardakaka484@gmail.com>
2021-01-17 11:15:46 +0300

    first commit

--- insta/settings.py
+++ insta/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import cloudinary
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,14 +32,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gram',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gram',
-    'bootstrap4',
+    'crispy_forms',
+    'cloudinary',
+    
 ]
 
 MIDDLEWARE = [
@@ -80,11 +84,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'igers',
         'USER': 'postgres',
-      'PASSWORD':'20302000b',
+    'PASSWORD':'20302000b',
+    'HOST':'127.0.0.1',
+    'PORT':'5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -122,3 +126,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+
+cloudinary.config(
+  cloud_name = "dvt3d0x7d",
+  api_key = "757462598898596",
+  api_secret = "xB39A-8OALx6n0x3H-sk9raOmVI"
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+RISPY_TEMPLATE_PACK='bootstrap4'
+
+LOGIN_REDIRECT_URL='gram-landing'
+LOGIN_URL='login'
